f25f01b6dbf74d77ab3ee2f8e9f6077ae14948ee
drishtipala <105123081+drishtipala@users.noreply.github.com>
2022-06-09 14:29:15 +0530

    GRT, Sum_of_digit, Even_Odd

---
+++ Class_three.java
@@ -0,0 +1,14 @@
+package functions;
+
+public class Class_three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class_one obj = new Class_one();
+		Class_two obj1 = new Class_two();
+		
+		obj1.sum();
+        obj.GRT();
+        obj.Even_Odd();
+	}
+}
